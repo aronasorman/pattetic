3e26cd676b44e2cd35232212253a948baa9a4fc8
Aron Fyodor M. Asor <aronasorman@gmail.com>
2012-09-24 09:28:57 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pattetic::Application.config.secret_token = 'd89f33cadb2f0b25809c7deb4577937f515dcf943f7b661cd3104757435a3d9db082b7a23a2781192d3758fc7e87fc7779797fe03039dcf319c0fb7cc5bc00cc'
